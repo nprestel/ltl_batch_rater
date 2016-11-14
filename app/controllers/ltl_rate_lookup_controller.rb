3e935a1637a2f887ac170cec61ff3ff9c1d54d94
Nick Prestel <nick.prestel@gmail.com>
2016-11-14 15:22:38 -0500

    Add a LTL Rate Lookup controller

---
+++ app/controllers/ltl_rate_lookup_controller.rb
@@ -0,0 +1,2 @@
+class LtlRateLookupController < ApplicationController
+end
---
+++ test/controllers/ltl_rate_lookup_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LtlRateLookupControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
