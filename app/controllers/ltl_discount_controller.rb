1cc9fff768831eefe35c61de4b9d726ea14a2a57
Nick Prestel <nick.prestel@gmail.com>
2016-11-17 13:12:10 -0500

    Created LTL Discount Controller

---
+++ app/controllers/ltl_discount_controller.rb
@@ -0,0 +1,11 @@
+class LtlDiscountController < ApplicationController
+
+	def index
+	    @ltl_discounts = LtlDiscount.order(:origin_state, :dest_state)
+
+	    respond_to do |format|
+	      format.html
+	      format.csv { send_data @ltl_discounts.to_csv }
+	    end
+  	end
+end
---
+++ test/controllers/ltl_discount_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LtlDiscountControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
