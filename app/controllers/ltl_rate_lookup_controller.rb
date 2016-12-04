8ebf1988a35cb0002f69f19d93c31572c7386b2d
Nick Prestel <nick.prestel@gmail.com>
2016-12-04 12:08:30 -0500

    fixed LTL discount logic

--- app/controllers/ltl_rate_lookup_controller.rb
+++ app/controllers/ltl_rate_lookup_controller.rb
@@ -7,7 +7,7 @@ class LtlRateLookupController < ApplicationController
     	@zip_code1 = ZipCode.get_zip(params[:origin_zip]) if params[:origin_zip].present?
     	@zip_code2 = ZipCode.get_zip(params[:dest_zip]) if params[:dest_zip].present?
 
-    	#@ltl_discount = LtlDiscount.get_discount(@zip_code1.state, @zip_code2.state) if @zip_code1 != "NO MATCH" && @zip_code2 != "NO MATCH"
+    	@ltl_discount = LtlDiscount.get_discount(@zip_code1.state, @zip_code2.state) if params[:origin_zip].present? && params[:dest_zip].present? && @zip_code1 != "NO MATCH" && @zip_code2 != "NO MATCH"
     	#@transit = Transit.find_by(origin_state: ZipCode.new(params[:origin_zip]).state, dest_state: ZipCode.new(params[:dest_zip]).state)
   		end
 end
