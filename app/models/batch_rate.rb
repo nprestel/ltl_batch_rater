b3fa03bec981d7d59dac5ff2b7674423bd770d9f
Nick Prestel <nick.prestel@gmail.com>
2016-12-12 19:13:34 -0500

    finished import validations, still needs refactoring for speed

--- app/models/batch_rate.rb
+++ app/models/batch_rate.rb
@@ -39,17 +39,26 @@ class BatchRate < ApplicationRecord
 				BatchRate.create(:shipmentID => row['shipmentID'], :error_code => "WEIGHT MUST BE 1-15,000 LBS")
 			elsif (row['orig_5zip'].blank? || row['dest_5zip'].blank?) #check if either zips are blank
 				BatchRate.create(:shipmentID => row['shipmentID'], :error_code => "MISSING ZIP CODE")
+			elsif !(/\A\d{5}\z/ === row['orig_5zip']) || !(/\A\d{5}\z/ === row['dest_5zip']) #check if either zips are non-5 digit numeric strings
+				BatchRate.create(:shipmentID => row['shipmentID'], :error_code => "ZIP MUST BE 5 DIGITS")
 			elsif (ZipCode.get_zip(row['orig_5zip']) == "NO MATCH" || ZipCode.get_zip(row['dest_5zip']) == "NO MATCH") #check if either zips are invalid
 				BatchRate.create(:shipmentID => row['shipmentID'], :error_code => "INVALID ZIP CODE")
-			else
-				BatchRate.create(:shipmentID => row['shipmentID'], :orig_5zip => row['orig_5zip'], :orig_state => ZipCode.get_zip(row['orig_5zip']).state, :dest_5zip => row['dest_5zip'], :dest_state => ZipCode.get_zip(row['dest_5zip']).state, :weight => row['weight'].to_i)
+			else #if all validations pass, set BatchRate attributes, NOTE: SCAC & FAK values given because model isn't instantiated and values don't exist
+					if Rate.get_rate('CTII', row['orig_5zip'], row['dest_5zip'], '70', row['weight']) == "NO MATCH"
+						BatchRate.create(:shipmentID => row['shipmentID'], :error_code => "LANE NOT IN TARIFF")
+					elsif LtlDiscount.get_discount(ZipCode.get_zip(row['orig_5zip']).state, ZipCode.get_zip(row['dest_5zip']).state) == "NO MATCH"
+						BatchRate.create(:shipmentID => row['shipmentID'], :orig_5zip => row['orig_5zip'], :orig_state => ZipCode.get_zip(row['orig_5zip']).state, :dest_5zip => row['dest_5zip'], :dest_state => ZipCode.get_zip(row['dest_5zip']).state, :weight => row['weight'].to_i, :charge => Rate.get_rate('CTII', row['orig_5zip'], row['dest_5zip'], '70', row['weight']), :error_code => "NO DISCOUNT EXISTS FOR LANE")
+					else BatchRate.create(:shipmentID => row['shipmentID'], :orig_5zip => row['orig_5zip'], :orig_state => ZipCode.get_zip(row['orig_5zip']).state, :dest_5zip => row['dest_5zip'], :dest_state => ZipCode.get_zip(row['dest_5zip']).state, :weight => row['weight'].to_i, :discount => LtlDiscount.get_discount(ZipCode.get_zip(row['orig_5zip']).state, ZipCode.get_zip(row['dest_5zip']).state).first.discount.to_f, :charge => Rate.get_rate('CTII', row['orig_5zip'], row['dest_5zip'], '70', row['weight']), :min => LtlDiscount.get_discount(ZipCode.get_zip(row['orig_5zip']).state, ZipCode.get_zip(row['dest_5zip']).state).first.min.to_f)
+					end
 			end
 		end
+
+		BatchRate.where(:error_code => "NONE").update_all("disc_charge = (CAST(charge as float) * (1 - CAST(discount as float)))")
 
 	end
 
 	def self.to_csv(options = {})  # EXPORTS RECORDS TO CSV
-	    desired_columns = ["shipmentID", "carrier_scac", "nmfc_class", "orig_5zip", "orig_state", "orig_country", "dest_5zip", "dest_state", "dest_country" "weight", "disc_charge", "discount", "charge", "min", "error_code"]
+	    desired_columns = ["shipmentID", "carrier_scac", "nmfc_class", "orig_5zip", "orig_state", "orig_country", "dest_5zip", "dest_state", "dest_country", "weight", "disc_charge", "discount", "charge", "min", "error_code"]
 	    CSV.generate(options) do |csv|
 	      csv << desired_columns
 	      all.each do |batch_rates|
