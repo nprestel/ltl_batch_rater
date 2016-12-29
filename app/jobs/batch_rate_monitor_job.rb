92d5941cd23bcb2bfbb9c03802244b2da9234a64
Nick Prestel <nick.prestel@gmail.com>
2016-12-29 00:28:44 -0500

    update to BatchRateMonitorJob to better handle file path for Heroku

--- app/jobs/batch_rate_monitor_job.rb
+++ app/jobs/batch_rate_monitor_job.rb
@@ -1,8 +1,11 @@
+require 'open-uri'
+
 class BatchRateMonitorJob < ApplicationJob
   queue_as :default
 
   def perform(file_path)
-    BatchRate.import_rates(file_path)
+  	csv_file = open(file_path,'rb:UTF-8')
+    BatchRate.import_rates(csv_file)
   end
 end
 
